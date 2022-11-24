a5bac0f515e47edcf15f52a6f3cbdcf1df61006d
Gsaranya434 <saranya16434@gmail.com>
2022-11-24 10:40:06 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,131 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
-function App() {
+
+
+
+function App() {    
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Tag Space Design Document</h1>      
+      <nav className="navbar navbar-expand-lg bg-light">
+        <div className="container-fluid">
+          <a className="navbar-brand" href="#">Creattie</a>
+          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+              <li className="nav-item">
+                <a className="nav-link active" aria-current="page" href="#">Lottie</a>
+                <ul className="dropdown-menu">
+                  <li><a className="dropdown-item" href="#">Action</a></li>
+                  <li><a className="dropdown-item" href="#">Another action</a></li>
+                  <li><hr className="dropdown-divider"></hr></li>
+                  <li><a className="dropdown-item" href="#">Something else here</a></li>
+                </ul>
+              </li>
+              <li className="nav-item"> 
+                <a className="nav-link" href="#">Vector</a>
+              </li>
+              <li className="nav-item dropdown">
+                <a className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                  Free
+                </a>                
+              </li>              
+            </ul>
+            <form className="d-flex" role="search">
+              {/* <select defaultValue="0">
+                <option value="1" selected hidden>All</option>
+              </select> */}
+              <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search"></input>
+              <i className="fa fa-search" aria-hidden="true"></i>
+              <button className="btn btn-outline-success" type="submit">Search</button>
+            </form>
+            <button className="btn btn-outline-success" type="submit">Sign In</button>
+            
+          </div>
+        </div>
+      </nav>      
+      
+      <div className="row">
+        <div className="col">
+          <div className="card">
+            <div className="card-body">
+              <h5 className="card-title">Card title</h5>
+              <h6 className="card-subtitle mb-2 text-muted">Lottie Animations</h6>
+              <p className="card-text">Achieve more with lightweight Lottie animations</p>                
+            </div>
+          </div>
+        </div>
+        <div className="col"><div className="card">
+            <div className="card-body">
+              <h5 className="card-title">Card title</h5>
+              <h6 className="card-subtitle mb-2 text-muted">Illustrations</h6>
+              <p className="card-text">Handpicked illustrations for any mission</p>                
+            </div>
+          </div>
+        </div>
+        <div className="col">
+          <div className="card">
+            <div className="card-body">
+              <h5 className="card-title">Card title</h5>
+              <h6 className="card-subtitle mb-2 text-muted">Animated Icons</h6>
+              <p className="card-text">The fastest growing database of motion icons</p>                
+            </div>
+          </div>
+        </div>
+        <div className="col">
+          <div className="card">
+            <div className="card-body">
+              <h5 className="card-title">Card title</h5>
+              <h6 className="card-subtitle mb-2 text-muted">Icons</h6>
+              <p className="card-text">Curated collections for any industry</p>                
+            </div>
+          </div>
+        </div>
+      </div>
+      <div>Top lottie animation</div>
+      <div className="row">
+        <div className="col">
+          <div className="card">
+            <div className="card-body">
+              <h5 className="card-title">Card title</h5>
+              <h6 className="card-subtitle mb-2 text-muted">Lottie Animations</h6>
+              <p className="card-text">Achieve more with lightweight Lottie animations</p>                
+            </div>
+          </div>
+        </div>
+        <div className="col"><div className="card">
+            <div className="card-body">
+              <h5 className="card-title">Card title</h5>
+              <h6 className="card-subtitle mb-2 text-muted">Illustrations</h6>
+              <p className="card-text">Handpicked illustrations for any mission</p>                
+            </div>
+          </div>
+        </div>
+        <div className="col">
+          <div className="card">
+            <div className="card-body">
+              <h5 className="card-title">Card title</h5>
+              <h6 className="card-subtitle mb-2 text-muted">Animated Icons</h6>
+              <p className="card-text">The fastest growing database of motion icons</p>                
+            </div>
+          </div>
+        </div>
+        <div className="col">
+          <div className="card">
+            <div className="card-body">
+              <h5 className="card-title">Card title</h5>
+              <h6 className="card-subtitle mb-2 text-muted">Icons</h6>
+              <p className="card-text">Curated collections for any industry</p>                
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
+    
   );
 }
 
